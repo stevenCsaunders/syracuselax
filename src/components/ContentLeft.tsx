a7a82ba54fdc2dc6e7f0d0985cd641e0fdb9a93a
Steven Saunders <stevensaunders@Stevens-MacBook-Air.local>
2021-11-15 13:53:39 -0700

    adding content and spacing on mobile

--- src/components/ContentLeft.tsx
+++ src/components/ContentLeft.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import logo from "../images/logo.svg";
 
 export default function ContentLeft() {
-    return (
-        <div>
-            <p>Content Left</p>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center px-4">
+      <h3 className="text-2xl text-black pb-4">Title Here for SYLAX Intro</h3>
+      <img src={logo} alt="Youth Lacrosse Player" className="" />
+      <h3 className="text-2xl text-black pb-4">Title Here for SYLAX Intro</h3>
+      <p className="text-center">
+        The Syracuse Youth LAX Program has expanded and there is a great need
+        for community involvement. Increased youth players and parents are key
+        to success.
+      </p>
+    </div>
+  );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,17 @@ import NavBar from "./NavBar";
 
 const Header: React.FC = () => {
   return (
-    <header className="text-white text-center bg-intro bg-cover px-5 pt-10 rounded-bl-xxl">
+    <header className="text-white text-center bg-intro bg-cover px-5 pt-10 mb-16 rounded-bl-xxl">
       <NavBar />
       <h1 className="text-4xl text-white pb-4">Syracuse Youth Lacrosse</h1>
       <h2 className="text-lg font-light pb-12 text-white">
         The Syracuse Youth LAX Program is for boys and gilrs grades K-8
       </h2>
-      <div className="flex gap-4 justify-center pb-10">
-        <button className="px-6 py-3 bg-white text-blue rounded-full">
+      <div className="flex gap-4 justify-center pb-32">
+        <button className="px-6 py-2 bg-white text-blue rounded-full">
           Register
         </button>
-        <button className="border border-solid border-white px-6 py-3 rounded-full">
+        <button className="border border-solid border-white px-6 py-2 rounded-full">
           Contact
         </button>
       </div>

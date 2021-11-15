78f3b13c986331de92adca9144795d58d8aa7527
Steven Saunders <stevensaunders@stevens-air.lan>
2021-11-15 01:59:50 -0700

    mobile styles for header

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,23 +1,15 @@
-import React from 'react'
-
-import logo from '../images/LogoSm.png'
+import React from "react";
 
 const Nav: React.FC = () => {
-
     return (
-        <div className='flex'>
-            <img src={logo} alt='Titan sword logo with no text' className='w-16 h-16' />
-
-            {/* Move this in to a useEffect toshow/hide the nav box */}
-            <nav className='flex flex-col invisible'>
-                <ul>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                </ul>
-            </nav>
-        </div>
+        <nav className='flex flex-col invisible'>
+            <ul>
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+            </ul>
+        </nav>
     )
-}
+};
 
-export default Nav
+export default Nav;

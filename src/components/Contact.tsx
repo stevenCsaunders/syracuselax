43149bd6404038cfee458d0616eba7f3dd711be8
Steven Saunders <steve0183@gmail.com>
2021-12-11 14:39:45 -0700

    completed contact form to run through google account to info@syracuselax.org address

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import Header from './Header'
 import { IHeaderProps, IProps } from '../App'
 import contactImg from '../images/contact.png'
+import emailJs, { init } from 'emailjs-com'
+
+init('user_zxD5Nb0ISfZhPENNz1C2W')
 
 const Contact: React.FC<IProps & IHeaderProps> = ({
 	headerStyles,
@@ -13,8 +16,26 @@ const Contact: React.FC<IProps & IHeaderProps> = ({
 	const [message, setMessage] = useState('')
 	const [emailSent, setEmailSent] = useState(false)
 
-	const handleSubmit = () => {
+	const handleSubmit = (e: { preventDefault: () => void }) => {
+		e.preventDefault()
 		if (name && isValidEmail(email) && message) {
+			const serviceID = 'service_gmail'
+			const templateID = 'template_f62lf8c'
+			const templateParams = {
+				name,
+				email,
+				message,
+			}
+
+			emailJs
+				.send(
+					serviceID,
+					templateID,
+					templateParams
+				)
+				.then((response) => console.log(response.text))
+				.then((error) => console.log(error))
+
 			setName('')
 			setEmail('')
 			setMessage('')
@@ -33,7 +54,8 @@ const Contact: React.FC<IProps & IHeaderProps> = ({
 	}
 
 	const inputStyle =
-		'py-3 px-4 rounded-xl  border border-blue bg-transparent text-blue placeholder-blue placeholder-opacity-50 mb-4'
+		'py-3 px-4 rounded-xl  border border-blue bg-transparent text-blue placeholder-blue placeholder-opacity-50 mb-6 mt-2'
+	const labelStyle = 'text-blue text-xl font-thin pl-2 border-l border-blue border-dotted'
 
 	return (
 		<>
@@ -54,39 +76,47 @@ const Contact: React.FC<IProps & IHeaderProps> = ({
 						alt='Youth Lacrosse Player'
 						className='xl:w-4 rounded-2xl mb-10 xl:ml-20 shadow-md'
 					/>
-					<div className='flex flex-col w-full '>
-						<input
-							className={inputStyle}
-							type='text'
-							placeholder='Your Name'
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-						/>
-						<input
-							className={inputStyle}
-							type='email'
-							placeholder='Your email address'
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-						<textarea
-							className={`${inputStyle} h-40`}
-							placeholder='Your message'
-							value={message}
-							onChange={(e) => setMessage(e.target.value)}
-						></textarea>
-						<button
-							className='border border-solid text-white bg-blue px-6 py-2 rounded-full hover:bg-transparent hover:text-blue md:px-10 md:w-5'
-							onClick={handleSubmit}
-						>
-							Send Message
-						</button>
-						<span
-							className={`${!emailSent ? 'hidden' : 'visible'}`}
-						>
-							Thank you for your message, we will be in touch
-							ASAP!
-						</span>
+					<div className='w-full '>
+						<form className='flex flex-col' onSubmit={handleSubmit}>
+							<label className={labelStyle}>Name</label>
+							<input
+								className={inputStyle}
+								name='name'
+								type='text'
+								placeholder='Your Name'
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+							<label className={labelStyle}>Email</label>
+							<input
+								className={inputStyle}
+								name='email'
+								type='email'
+								placeholder='Your email address'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							<label className={labelStyle}>Message</label>
+							<textarea
+								className={`${inputStyle} h-40`}
+								name='message'
+								placeholder='Your message'
+								value={message}
+								onChange={(e) => setMessage(e.target.value)}
+							></textarea>
+							<input
+								type='submit'
+								value='Send'
+								className='border border-solid text-white bg-blue px-6 py-2 rounded-full hover:bg-transparent hover:text-blue md:px-10 md:w-5'
+							/>
+							<span
+								className={`${
+									!emailSent ? 'hidden' : 'visible'
+								}`}
+							>
+								Thank you for your message, we will be in touch soon!
+							</span>
+						</form>
 					</div>
 				</div>
 			</div>

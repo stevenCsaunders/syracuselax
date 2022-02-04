449becb9add066fbdb13a0bdea64104825291f5b
Steven Saunders <steve0183@gmail.com>
2022-02-03 22:21:28 -0700

    update for youth clinic

--- src/components/ContentRight.tsx
+++ src/components/ContentRight.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import contentRight from '../images/content-right.png'
+import FileLink from '../utils/baseLinks'
 
 const ContentRight: React.FC = () => {
+
+	const handleFileOpen = (fileName: string) => {
+		window.open(fileName)
+	}
+
 	return (
 		//POSSIBLE IMPLEMENTATION -  object-fit: cover; object-position: bottom;
 		<div className='flex flex-col items-center px-4 mb-32 text-center bg-circles bg-fixed bg-left-bottom  bg-no-repeat md:mb-32 md:px-0 md:flex-row'>
@@ -12,11 +18,26 @@ const ContentRight: React.FC = () => {
 			/>
 			<div className='md:text-left md:ml-10'>
 				<h3 className='text-3xl pb-4 font-bold'>
-					Tournaments and Camps 
+					Syracuse Youth Clinic
 				</h3>
-				<p>
-					Throughout the year, there are several youth lacrosse tournaments and camps held in Utah. This section of the site will be updated as more information becomes available on these topics, so check back closer to the spring 2022 season for info.
+				<p className='text-lg font-thin leading-6 mb-4'>
+					<strong>WHEN:</strong> February 17th, 2022 - 4:00 to 6:00 PM{' '}
+					<br />
+					<strong>WHERE:</strong> Syracuse High Lacrosse Field (west
+					of the stadium)
+					<br />
+					<strong>COST:</strong> $25.00 per participant
+					<br />
+					All youth welcome, ages K-8. Equipment provided!
 				</p>
+				<button
+					className='border border-solid border-blue px-6 py-2 rounded-full hover:bg-blue hover:text-white md:px-10'
+					onClick={() =>
+						handleFileOpen(FileLink('YouthClinicSyracuseHigh.pdf'))
+					}
+				>
+					More Info
+				</button>
 			</div>
 		</div>
 	)
